371005fef28ef3b90efedba58b1c58299e4db0b7
Maxime-Le-Goff <lgmaximemlg@gmail.com>
2023-05-29 17:01:01 +0200

    sensio bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
